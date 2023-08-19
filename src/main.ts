b2203639290296ef39fd9a66a545e33bd9842e59
diekof <ddonizetecorrea@gmail.com>
2023-08-19 12:38:11 -0400

    mensagens de erro

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost,  NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './util/setupswagger';
+import { AllExceptionsFilter } from './common/exception/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   setupSwagger(app);
 
   app.enableCors({
--- src/pessoa/pessoa.service.ts
+++ src/pessoa/pessoa.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { UUID } from 'crypto';
 import { PrismaService } from 'src/common/prisma/prisma.service';
@@ -12,16 +12,33 @@ export class PessoaService {
   create(createPessoaDto: Prisma.pessoaCreateInput) {
     return this.prisma.pessoa.create({
       data: createPessoaDto,
+      }).then((pessoa) => {
+        return this.prisma.pessoa.findUnique({
+          where: { id: pessoa.id },
+          });
+      }).catch((error) => {
+        throw new BadRequestException('Ops! Ocorreu um erro ao salvar a pessoa.');
       });
   }
 
   findAll(pageOptionsDto: PaginateOptions) {
-    return paginator(pageOptionsDto)(this.prisma.pessoa);
+    return paginator(pageOptionsDto)(this.prisma.pessoa).then((pessoas) => {
+      return pessoas;
+    }).catch((error) => {
+      throw new BadRequestException('Ops! Ocorreu um erro ao buscar as pessoas.');
+    });
   }
 
   findOne(id: UUID) {
     return this.prisma.pessoa.findUnique({
       where: { id },
+      }).then((pessoa) => {
+        if (!pessoa) {
+          throw new BadRequestException('Ops! Pessoa não encontrada.');
+        }
+        return pessoa;
+      }).catch((error) => {
+        throw new BadRequestException('Ops! Ocorreu um erro ao buscar a pessoa.');
       });
   }
 
@@ -29,12 +46,16 @@ export class PessoaService {
     return this.prisma.pessoa.update({
       where: { id },
       data: updatePessoaDto,
+      }).catch((error) => {
+        throw new BadRequestException('Ops! Ocorreu um erro ao atualizar a pessoa.');
       });
   }
 
   remove(id: UUID) {
     return this.prisma.pessoa.delete({
       where: { id },
+      }).catch((error) => {
+        throw new BadRequestException('Ops! Ocorreu um erro ao remover a pessoa.'	);
       });
   }
 }
