db68d6aabc4eb6ad109f64994458892de6392843
Diego Donizete Correa da Silva <ddonizetecorrea@gmail.com>
2023-11-29 22:16:42 -0400

    fix: ajuste sem graca

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    "@commitlint/config-conventional"
   ],
   root: true,
   env: {
